d69447705efed2eaabbaa1ba0885c09ee479c057
cillee <9972218+cillee@user.noreply.gitee.com>
2024-11-22 22:01:40 +0800

    Independently and sequentially run.

--- convex-optimization/K-armed-bandit/Tsallis-Inf++.py
+++ convex-optimization/K-armed-bandit/Tsallis-Inf++.py
@@ -1,7 +1,6 @@
-from turtledemo.penrose import start
-
 import numpy as np
 
+# Haven't used
 def Lagrange_Min_P(p, eta, c_hat, v, alpha, It):
     p_min = (p ** (1/alpha - 1) + eta * c_hat + eta * v) ** (alpha / (1 - alpha))
 
@@ -13,25 +12,22 @@ def Lagrange_Min_P(p, eta, c_hat, v, alpha, It):
     value += v * (np.sum(p_min) - 1)
     return value, p_min
 
+
 def KKT(p, eta, c_hat, v, alpha, It):
     p_min = (p ** (1 / alpha - 1) + eta * c_hat + eta * v) ** (alpha / (1 - alpha))
     original_feasible = np.sum(p_min) - 1
     return p_min, original_feasible
 
-alpha = 2
-K = 20
-T = 10000
-eta = 1/np.sqrt(T)
-epsilon = 1e-6
 
-C = np.loadtxt('Loss_matrix.txt')
+def tsallis_inf_once(C, alpha, epsilon, p=None):
+    T, K = C.shape
+    eta = 1/np.sqrt(T)
+    if p is None:
+        p = np.full(K, 1 / K)
+    result_cost = 0
+    fix_choice_cost = np.zeros(K)
 
-p = np.full(K, 1/K)
-result_cost = 0
-fix_choice_cost = np.zeros(K)
-
-for _ in range(10):
-    for i in range(C.shape[0]):
+    for i in range(T):
         It = np.random.choice(K, p=p)
         c = C[i, :]
         result_cost += c[It]
@@ -57,9 +53,36 @@ for _ in range(10):
         v = (start + end) / 2
         p, _ = KKT(p, eta, c_hat, v, alpha, It)
         p = p / np.sum(p)
+    return p, result_cost, fix_choice_cost
 
 
-print("p_10001:", p)
-print("result cost:", result_cost / 10)
-print("fix choice cost:", fix_choice_cost / 10)
+if __name__ == '__main__':
+    C = np.loadtxt('Loss_matrix.txt')
+    T, K = C.shape
+    alpha = 2
+    epsilon = 1e-5
+
+    print("Independently run 10 times.")
+    result_p = np.zeros(K)
+    result_cost = 0
+    fix_choice_cost = np.zeros(K)
+    for _ in range(10):
+        result_p_i, result_cost_i, fix_choice_cost_i = tsallis_inf_once(C, alpha, epsilon)
+        result_p += result_p_i
+        result_cost += result_cost_i
+        fix_choice_cost += fix_choice_cost_i
+    print("p_10001:", result_p / 10)
+    print("result cost:", result_cost / 10)
+    print("fix choice cost:", fix_choice_cost / 10)
+
+    print("")
+    print("Sequentially run 10 times.")
+    p = np.full(K, 1 / K)
+    for _ in range(10):
+        p, result_cost, fix_choice_cost = tsallis_inf_once(C, alpha, epsilon, p=p)
+    print("p_10001:", p)
+    print("result cost:", result_cost)
+    print("fix choice cost:", fix_choice_cost)
+
+
 
